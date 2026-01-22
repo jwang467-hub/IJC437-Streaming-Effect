eba3129b2a226090c230ae8a54e2b184999e2630
jwang467-hub <jwang467@sheffield.ac.uk>
2026-01-22 03:12:04 +0000

    Add era definition script

--- code/era.R
+++ code/era.R
@@ -1,1 +1,20 @@
+# Create Era Variable for Billboard Data
 
+library(tidyverse)
+
+# Load data
+df <- read_csv("billboard_24years_lyrics_spotify.csv") %>%
+  mutate(year = as.numeric(year))
+
+# Define music industry eras
+df <- df %>%
+  mutate(
+    Era = case_when(
+      year >= 2000 & year <= 2010 ~ "Download Era",
+      year >= 2018 & year <= 2023 ~ "Streaming Era",
+      TRUE ~ "Transition Period"
+    )
+  )
+
+# Check era distribution
+table(df$Era)
